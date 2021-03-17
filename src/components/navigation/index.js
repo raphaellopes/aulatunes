30cb889300c09d35032631f27f71e9ee9d9ecdd8
Raphael Lopes <ph.campanario07@gmail.com>
2021-03-17 12:02:19 -0300

    add props from navigation component

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Container, MenuItem } from './styles';
 
-export const Navigation = () => (
-  <Container>
-    <MenuItem variant="primary">Albuns</MenuItem>
-    <MenuItem>Songs</MenuItem>
-    <MenuItem>Favorites</MenuItem>
-  </Container>
-);
+export const Navigation = ({ options, onClickOption }) => {
+  const renderOption = ({ label, value, variant }) => (
+    <MenuItem
+      key={`menu-option-${value}`}
+      variant={variant}
+      onClick={() => onClickOption(value)}
+    >
+      {label}
+    </MenuItem>
+  );
+
+  return (
+    <Container>
+      {options.map(renderOption)}
+    </Container>
+  );
+};
+
+Navigation.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string,
+    value: PropTypes.string,
+    variant: PropTypes.oneOf(['default', 'primary']),
+  })).isRequired,
+  onClickOption: PropTypes.func.isRequired,
+};
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
-// @TODO: Rename this page to Main and implements the routes here
 import {
   Header, Container, Navigation, Input,
 } from '../../components';
 
-const Home = () => (
-  <>
-    <Header />
-    <Container>
-      <Navigation />
-      <Input placeholder="Search" />
-    </Container>
-  </>
-);
+// @TODO: Rename this page to Main and implements the routes here
+const Home = () => {
+  const menuOptions = [
+    { label: 'Albums', value: 'album', variant: 'primary' },
+    { label: 'Songs', value: 'songs', variant: 'default' },
+    { label: 'Favorites', value: 'favorites', variant: 'default' },
+  ];
+
+  const handleClickMenuOption = (value) => console.log('handleClickMenuOption', { value });
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <Navigation options={menuOptions} onClickOption={handleClickMenuOption} />
+        <Input placeholder="Search" />
+      </Container>
+    </>
+  );
+};
 
 export default Home;
